bb4f95cf3258d9fae6ebc3e1cd05f77a52fe2df0
gauravsachdeva <sachdevag8944@gmail.com>
2025-06-07 15:22:43 +0530

    1. Easy level programs in Java 2. Star Patterns printing

--- src/PracticeProblemsEasyLevel/PrintNumberMultipleTimes.java
+++ src/PracticeProblemsEasyLevel/PrintNumberMultipleTimes.java
@@ -7,10 +7,29 @@ num: The number to be printed.
 n: The number of times num should appear.
 Your task is to print num, exactly n times, in a single line separated by spaces.
 
+Example 1:
+
+Input:
+
+Input: num = 5, n = 3
+Output:
+
+5 5 5
+Explanation:The number 5 is printed 3 times.
 */
+
+import java.util.Scanner;
 
 public class PrintNumberMultipleTimes {
     public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int number=in.nextInt();
+        System.out.print("Enter number of occurances: ");
+        int occurrences=in.nextInt();
 
+        for(int i=0;i<occurrences;i++){
+            System.out.print(number+" ");
+        }
     }
 }
