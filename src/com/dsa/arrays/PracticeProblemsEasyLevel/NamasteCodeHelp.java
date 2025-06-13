6fdbe79cb41f1ea2501194f66e7334131458c8e9
gauravsachdeva <sachdevag8944@gmail.com>
2025-06-13 07:25:53 +0530

    Create the package and move the files

--- src/com/dsa/arrays/PracticeProblemsEasyLevel/NamasteCodeHelp.java
+++
@@ -1,7 +0,0 @@
-package com.dsa.arrays.PracticeProblemsEasyLevel;
-
-public class NamasteCodeHelp {
-    public static void main(String[] main){
-        System.out.print("@@"+"Namaste Codehelp"+"@@");
-    }
-}
